bcb417946768f6b823aa6e2f92dd34f5fb49f475
Nagu@126 <nagubabu126@gmail.com>
2022-01-24 00:04:45 -0800

    first commmit on createObject

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,17 @@
 function CreateObject(arr) {
     // Write your code here
+   var resObj= {}
+    for (i=0;i<arr.length;i++){
+        if (i%2===0){
+           var key=arr[i]
+        }
+        else{
+           var value=arr[i]
+            resObj[key]=value
+        }
+    }
+
+    return resObj
 }
 
 module.exports = CreateObject;
